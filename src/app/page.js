cad0322ddef42bc36a63bca69ce6741192654206
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-08-27 21:07:26 +0900

    initial

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js Hello World!!!!!</code>
+          <code className={styles.code}>src/app/page.js Hello World!</code>
         </p>
         <div>
           <a
