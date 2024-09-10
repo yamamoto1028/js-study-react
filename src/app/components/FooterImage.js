6758ea0add0a026cfc88eaffd301d0c7f121e28d
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-09-10 23:25:23 +0900

    💄 スタイルファイルの分割

---
+++ src/app/components/FooterImage.js
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
+export function FooterImage(props) {
+  return (
+    <footer className={styles.Footer}>
+      {props.text}
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className={styles.vercelLogo}
+        width={100}
+        height={24}
+        priority
+      />
+    </footer>
+  );
+}
