8048c3c78babe653345579ee6495a39b67b4d48a
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-09-11 23:16:10 +0900

    ✨ ヘッダーの追加

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 "use client";
 import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import "./page.module.css";
 
 export default function Index() {
   return (
     <div>
+      <Header />
       <Main page="index" />
       <Footer />
     </div>
