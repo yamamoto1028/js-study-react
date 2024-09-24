e29891da44fb081b616fb97a403bdc0df8606024
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-09-24 10:59:20 +0900

    🎨 設定の記載箇所を変更。page.jsxをクライアントコンポーネントにした

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,8 @@
+"use client";
 import { Footer } from "@/app/components/Footer";
 import { Header } from "@/app/components/Header";
 import { Main } from "@/app/components/Main";
 import "@/app/about/about.module.css";
-
-export const metadata = {
-  title: "About Page",
-  description: "Learn more about our company on the about page",
-};
 
 export default function About() {
   return (
